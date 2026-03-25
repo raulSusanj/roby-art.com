6744eda289ad2708f2637c0fdd6b0f0b52ee665f
raulsusanj <rsusanj@dickeys.com>
2026-03-25 22:57:12 +0100

    Allow public read access on products collection

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -2,6 +2,9 @@ import type { CollectionConfig } from 'payload'
 
 export const Products: CollectionConfig = {
   slug: 'products',
+  access: {
+    read: () => true,
+  },
   fields: [
     {
       name: 'name',
