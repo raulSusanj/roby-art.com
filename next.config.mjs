2ca17c398bfab4249e1cc4015b68bf46b4797b9f
raulsusanj <rsusanj@dickeys.com>
2026-03-26 00:38:14 +0100

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -13,4 +15,4 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+export default withPayload(nextConfig, { devBundleServerPackages: false })
