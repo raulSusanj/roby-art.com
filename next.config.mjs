91a8622eb0c3657aa223a22deccc605f771aa8af
raulsusanj <rsusanj@dickeys.com>
2026-03-26 00:23:02 +0100

    removed local images

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,16 @@
-import { withPayload } from '@payloadcms/next/withPayload'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.public.blob.vercel-storage.com', // Wildcard for all Vercel Blob stores
+        hostname: '**.public.blob.vercel-storage.com',
       },
     ],
     domains: [
-      'www.roby-art.com', // Your domain
-      // Add other domains if needed (e.g., 'localhost' for development)
+      'www.roby-art.com',
     ],
   },
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default nextConfig
