c7686790f5fbd8546d2b59ceca4a114ecd94219a
raulsusanj <rsusanj@dickeys.com>
2026-03-25 23:57:20 +0100

    Revert "Allow public read access on products collection"
    This reverts commit 6744eda289ad2708f2637c0fdd6b0f0b52ee665f.

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -2,9 +2,6 @@ import type { CollectionConfig } from 'payload'
 
 export const Products: CollectionConfig = {
   slug: 'products',
-  access: {
-    read: () => true,
-  },
   fields: [
     {
       name: 'name',
